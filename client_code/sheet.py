6e25fa57fd6dd7f2e2edc831512aef4e85b792dd
lamarasingham@icloud.com <lamarasingham@icloud.com>
2026-03-21 17:05:18 +0000

    Created sheet

---
+++ client_code/sheet.py
@@ -0,0 +1,24 @@
+import anvil.server
+import anvil.facebook.auth
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+# This is a module.
+# You can define variables and functions here, and use them from any form. For example, in a top-level form:
+#
+#    from .. import Module1
+#
+#    Module1.say_hello()
+#
+
+def getusrs():
+  return app_files.users['usr'].rows
+def backusr(usr):
+  db=app_files.user['usr']
+  x=None
+  for i in getusrs():
+    if i['email']==usr:
+      
