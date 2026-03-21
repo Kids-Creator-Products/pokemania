6d02ba3912a36f5ed7434cb40a9c8d1648281386
lamarasingham@icloud.com <lamarasingham@icloud.com>
2026-03-21 17:56:44 +0000

    Edited sheet

--- client_code/sheet.py
+++ client_code/sheet.py
@@ -16,6 +16,7 @@ from anvil.tables import app_tables
 
 def getusrs():
   return app_files.users['usr'].rows
+  """
 def backusr(usr):
   db=app_files.users['usr']
   for i in db.rows:
@@ -26,3 +27,24 @@ def backusr(usr):
   for k in d:
     x[k]=str(d[k])
   db.add_row(x)
+  """
+def backusr(usr_email):
+  # 1. Get the current logged-in user
+  user = anvil.users.get_user()
+  if not user:
+    return "No user logged in"
+
+    # 2. Reference your storage table
+  db = app_tables.users # Assuming 'users' is an Anvil Data Table
+
+  # 3. Look for an existing record
+  existing_row = db.get(email=usr_email)
+
+  # 4. Prepare data (converting to string as you did)
+  x
+  user_data = {k: str(user[k]) for k in x}
+
+  if existing_row:
+    existing_row.update(**user_data)
+  else:
+    db.add_row(**user_data)
